8e88e1d0f701aee521f9749de2415686182b23f9
Thomas Keil <thomas@die-keils.info>
2018-09-05 17:36:48 +0200

    Mehr Daten, einheitlichere Länge, gefälligere Texte

--- src/timestamps.php
+++ src/timestamps.php
@@ -21,79 +21,123 @@ $periods = [
   [
     "period" => 1,
     "name" => "Die Papierhochzeit",
-    "text" => "Der Grundgedanke hinter dieser Bezeichnung ist, dass die Ehe noch frisch und dünn wie Papier ist. Zwar fühlen sich die meisten Paare schon sehr stark verbunden, doch könnten \"widrige Umstände\" die Ehe immer noch auflösen wie Nässe das Papier.\n Die Papierhochzeit ist einer der Hochzeitstage der meist in trauter Zweisamkeit gefeiert wird, um die Erinnerungen an die gemeinsame Zeit und den Tag der Trauung noch einmal wach zurufen."
+    "text" => "Wie jeder weiß, ist Papier nicht besonders stabil. So gilt auch die Partnerschaft nach einem Ehejahr noch lange nicht als gefestigt. Sie ist hauchdünn, wie Papier und kann leicht zerreißen. Dennoch haben die Eheleute das erste Ehejahr erfolgreich gemeistert. Das heißt natürlich nicht, dass es immer ein reines Honigschlecken war. Sicherlich musste das junge Paar schon so manche Klippe umschiffen. Daher sollten Sie auch das erste Ehejubiläum nicht ohne Worte unter den Tisch kehren."
     ],
   [
     "period" => 2,
     "name" => "Die Baumwollenhochzeit",
-    "text" => "Das Brautpaar hat die ersten Höhen und Tiefen der Ehe hinter sich und kann mit diesen gut umgehen. Dadurch festigt sich der neue Bund und das Paar fühlt sich geborgen. Der Name Baumwollhochzeit entstand, weil Baumwolle weich ist, gut wärmt und damit die Geborgenheit in einer guten Ehe darstellt."
+    "text" => "Obwohl die Baumwolle ein recht robuster Stoff ist, der sich bei sehr hohen Temperaturen waschen lässt, wird sie mit jeder Reinigung etwas dünner. Um diesen Vergleich nun auf die Baumwollene Hochzeit anzuwenden, heißt das: Die Ehe ist nach zwei gemeinsam verbrachten Jahren sicherlich schon relativ fest. Sie gerät auch durch stärkere Erschütterungen nicht aus den Fugen."
     ],
   [
     "period" => 3,
     "name" => "Die Lederne Hochzeit",
-    "text" => "Leder ist widerstandsfähig und verlässlich. Daher soll bei der Ledernen Hochzeit gefeiert werden, dass sich die Ehepartner kennen und lieben - sowohl die schönen als auch die weniger schönen Seiten. Sie haben gelernt mit den Stimmungen des Partners umzugehen und den anderen so zu nehmen, wie er ist."
+    "text" => "Nach drei Jahren Ehe ist die Beziehung nicht mehr so dünn, wie Papier. Sie wird nicht bei jedem kleinen Problem auf die Zerreißprobe gestellt, sondern ist mittlerweile so zäh, wie Leder. Allerdings sollten sich die Ehepartner bewusst sein, dass auch eine gefestigte Beziehung gepflegt werden will. Denn, ebenso wie Leder spröde wird, wenn Sie es vernachlässigen, leidet auch die Ehe, wenn sie nur noch vor sich hinplätschert."
     ],
   [
     "period" => 4,
     "name" => "Die Seiden Hochzeit",
-    "text" => "Ein nicht all zu bekannter Hochzeitstag und trotzdem wichtig genug um genannt zu werden. Dieser Tag wird gerne im kleinen Kreis verbracht und dazu genutzt, sich die Hochzeitsfotos noch einmal anzuschauen und ein bisschen in Erinnerung zu schwelgen. Falls doch größer gefeiert wird, bietet es sich an Seidenelemente mit in die Dekoration einzubauen. Eine Seidene Tischdecke mit passenden Servietten aus Seide und um alles abzurunden ein paar hübsche Seidenblumen, ganz getreu dem Motto."
+    "text" => "Die Seide gilt als die stärkste Naturfaser überhaupt. Daraus lässt sich schließen, dass nun, nach vier Jahren des Zusammenlebens auch die Ehe eine gewisse Stärke erreicht hat. Außerdem ist die Seide edel und kostbar. Und genau diese Eigenschaften treffen auch auf eine vierjährige, glückliche Ehe zu. Schließlich ist es heutzutage längst keine Selbstverständlichkeit, dass eine Ehe länger als drei Jahre hält."
     ],
   [
     "period" => 5,
     "name" => "Die Hölzerne Hochzeit",
-    "text" => "Holz ist stark, beständig und stabil. Nach fünf Ehejahren wird in vielen Fällen auch die Ehe so sein. Das ist die Aussage der Hölzernen Hochzeit. So wie das Holz soll auch die Ehe in den nächsten Jahren immer weiter wachsen und immer kräftiger werden. Die Hölzerne Hochzeit ist das erste runde Jubiläum der Hochzeitstage."
+    "text" => "Wenn ein Ehepaar 5 Jahre verheiratet ist, dann hat die Ehe Bestand. Sie ist so fest wie Holz und wird deshalb auch als Hölzerne Hochzeit bezeichnet. Um dem Jubelpaar eine weiterhin glückliche und harmonische Ehe zu wünschen, wird auf Holz geklopft."
     ],
   [
     "period" => 6,
     "name" => "Die Zinn Hochzeit",
-    "text" => "Wie Zinn sollte auch die Ehe von Zeit zu Zeit ein bisschen aufgeputzt und poliert werden. Das verleiht neuen Glanz und neuen Schwung. Weil Zinn sehr weich und  formbar ist, steht es sinnbildlich  für die Formbarkeit der Ehe. Nutzen Sie diesen Tag und probieren sie etwas Neues aus. Beispielsweise festigen Fallschirmsprünge, Wanderurlaube oder andere Aktivitäten das Miteinander und bringen Spaß und außergewöhnliche Erinnerungen."
+    "text" => "Wie Zinn sollte auch die Ehe von Zeit zu Zeit ein bisschen aufgeputzt und poliert werden. Das verleiht neuen Glanz und neuen Schwung. Weil Zinn sehr weich und formbar ist, steht es sinnbildlich für die Formbarkeit der Ehe. Nutzt diesen Tag und probiert etwas Neues aus."
     ],
   [
     "period" => 7,
     "name" => "Die Kupferne Hochzeit",
-    "text" => "Die Ehe glänzt nach sieben Jahren nicht mehr so neu, wie am ersten Tag. Aber unter der Schicht der Jahre liegt noch immer das \"Schöne\" und \"Wahre\". Aus der Verliebtheit ist eine innige Liebe geworden, die sich als Patina auf die Ehe gelegt hat."
+    "text" => "Kupfer ist ein leicht formbares Metall, das nach einigen Jahren Patina ansetzt. Ebenso gilt auch die Ehe nach sieben Jahren noch als leicht formbar. Kein Ehepaar sollte sich jetzt auf seinen Lorbeeren ausruhen. Wenn die Zärtlichkeiten weniger werden und die Streitigkeiten zunehmen, ist das der Anfang vom Ende. Dass eine Ehe in jedem Jahr in die Brüche gehen kann, darüber sollten sich alle Eheleute bewusst sein."
     ],
   [
     "period" => 8,
     "name" => "Die Blecherne Hochzeit",
-    "text" => "Nach acht Jahren ist in vielen Ehen der Alltag eingezogen. Das ist keineswegs etwas Negatives, auch wenn es im ersten Moment so klingt. Man versteht sich auch ohne Sprache, weiß, wie man miteinander umgehen muss, was man geben und was man nehmen kann und aus dem Ehepaar ist ein eingespieltes Team geworden."
+    "text" => "Als Blech wird in der Regel ein dünn gewalztes Metall bezeichnet. Um welche Art von Metall es sich dabei handelt ist nicht bestimmt. Aber durch die Dicke ist festgelegt, dass es sich um ein biegbares Metallstück handelt. Die Bezeichnung Blecherne Hochzeit heißt also, dass die Ehe des Jubelpaares zwar beständig ist, sich aber durchaus noch formen lässt. Nach acht Ehejahren haben sich die beiden Partner aneinander gewöhnt."
     ],
+  [
+      "period" => 9,
+      "name" => "Die Keramikhochzeit",
+      "text" => "Als Keramik werden in der Regel gebrannte Gebrauchs- oder Ziergegenstände bezeichnet. Das heißt, es handelt sich um Produkte, die sich im Rohzustand leicht verformen lassen, aber nach einer gewissen Zeit im Ofen hart und stabil sind. Sicherlich orientiert sich der Name Keramikhochzeit an diesen Eigenschaften des Materials. Denn nach 9 Ehejahren hat die Ehe bereits feste Formen angenommen. Die Gewohnheiten des Partners sind ebenso bekannt, wie seine kleinen Makel."
+      ],
   [
     "period" => 10,
     "name" => "Die Rosenhochzeit",
-    "text" => "Ein ganzes Jahrzehnt verheiratet, damit ist die Rosenhochzeit einer der wichtigen Hochzeitstage. Seite an Seite mit einem anderen Menschen. Da erlebt man viel Gutes und muss auch mal schlechte Tage teilen. Da ist es Zeit sich zu zeigen, dass man sich liebt.\nUnd wie geht das besser und romantischer als mit der Blume der Liebe, der Rose?\nBlüte und Dornen symbolisieren von der Liebe berührte Momente. Die duftende Blüte zeigt Reife und Schönheit, ihr Stiel mit den Dornen symbolisiert Zeiten in denen man auf Konflikte und belastete Zeiten zurückschauen muss."
+    "text" => "Bei vielen Menschen gilt die Rose als die Königin der Blumen, denn sie ist wunderschön. Gleichzeitig ist sie die Blume der Liebe, weshalb ihr auch das erste Zehnerjubiläum der Hochzeitstage gewidmet sein darf. Denn wer seinen 10. Hochzeitstag begeht, liebt sich wirklich. Längst wurde die rosarote Brille abgelegt. Der Partner wird so gesehen, geliebt und respektiert, wie er ist."
     ],
   [
     "period" => 11,
     "name" => "Die Stahlhochzeit",
-    "text" => "Elf Jahre verheiratet, da halten Mann und Frau im Volksmund so fest zusammen wie Kruppstahl. Der starke Zusammenhalt des Paares wird auch in den weiteren Ehejahren den Hochs und Tiefs standhalten. Stahl wird oft auch als Grundgerüst beim Hausbau verwendet.\nUnd genauso spiegelt die Stahlhochzeit die Standfestigkeit und Stabilität der Ehe wider, die ihrerseits auch auf guten Grund gebaut ist."
+    "text" => "Hart wie Stahl ist die Bedeutung, die der Ehe nach 11 gemeinsamen Jahren zugeschrieben wird. Sicherlich ist das nicht verkehrt, denn wer elf Ehejahre hinter sich hat, der hat gemeinsam mit seinem Partner schon so manche Höhen und Tiefen durchlebt. Dass die Ehe immer noch Bestand hat, ist ein deutliches Zeichen für ihre Härte."
     ],
   [
     "period" => 12,
-    "month" => 6,
-    "name" => "Die Petersilienhochzeit",
-    "text" => "Dieses Datum ist für viele Ehepaare etwas Besonderes. Da die Petersilienhochzeit die Halbzeit zur Silberhochzeit markiert, wird sie von vielen Paaren gerne gefeiert. Die Petersilie steht im übertragenden Sinne für das Gedeihen der Ehe in den kommenden Jahren, sie soll schmackhaft und grün bleiben."
+    "name" => "Die Nickelhchzeit",
+    "text" => "Nickel ist ein Metall, das wegen seiner hohen Widerstandsfähigkeit gegenüber Luft, Wasser, Säuren und Laugen gerne in viel benutzten Gegenständen zusammen mit anderen Metallen verarbeitet wird. Es gilt als sehr beständig und so bedeutet die Nickelhochzeit, dass auch die Ehe nach 12 Jahren eine gewisse Beständigkeit hat. Das Ehepaar kennt sich nach so vielen Jahren in- und auswendig und hat es gelernt, auch den Alltag zu meistern."
     ],
   [
     "period" => 13,
     "name" => "Veilchenhochzeit",
-    "text" => "Der 13. Hochzeitstag ist auch unter den Namen Maiglöckchen Hochzeit und Spitzenhochzeit bekannt. Wie auch immer man ihn bezeichnet, all die Namensgeber haben eines gemeinsam:\nSie zeigen, dass die Ehe verletzlich ist wie eine Pflanze oder so empfindlich wie feine Spitze. Sie muss gepflegt werden."
+    "text" => "Obwohl die meisten Menschen wahrscheinlich wissen, wie ein Veilchen aussieht, haben sie in freier Natur noch keines zu sehen bekommen. Das liegt daran, dass die kleine, blaue Blume, die wir uns so gern im Blumentopf aufs Fensterbrett stellen, eher im Verborgenen blüht. Sie ist unter Büschen oder am Waldrand zu finden. Und weil das Veilchen trotz seiner Schönheit recht unscheinbar und schlicht ist, steht es sogar in Gedichten als Symbol für Bescheidenheit."
     ],
   [
     "period" => 14,
     "name" => "Elfenbeinhochzeit",
-    "text" => "Elfenbein ist ein kostbares und rares Gut. Auch viele Ehen halten den Irrungen und Wirrungen der Zeit nicht Stand. Paare die diesen Tag erreicht haben, sollten ihre Ehe weiterhin pflegen wie das kostbare Elfenbein. Zwar ist das Elfenbein ein Symbol für Härte und Unzerstörbarkeit, es muss aber, ebenso wie eine gute Ehe, stets gepflegt werden."
+    "text" => "Elfenbein wird aus den Stoßzähnen von Nilpferden und Elefanten gewonnen. Damit diese zentnerschweren Tiere sich mit ihren Zähnen bekämpfen und verletzen können, muss das Material sehr hart sein. Wahrscheinlich ist die Bedeutung der Elfenbeinhochzeit genau diesem Umstand zu verdanken. Denn auch eine Ehe, die in 14 Jahren sicher nicht immer nur Zuckerschlecken war, muss sehr hart im Sinne von stark sein, um so lange zu bestehen."
     ],
   [
     "period" => 15,
     "name" => "Die Gläserne Hochzeit",
-    "text" => "Dieser Tag steht nicht etwas für Zerbrechlichkeit, im Gegenteil. Die Gläserne Hochzeit zeigt, dass die Eheleute fest miteinander verbunden sind. Man weiß, was der andere denkt und führt."
+    "text" => "Nach einem lustigen Brauch wird zur Kristallhochzeit der Garten des Paares mit Girlanden aus Glasflaschen geschmückt. Der Hof vor dem Haus oder der Weg zur Haustüre wird ebenfalls gerne mit Flaschen vollgestellt. Wegen dieser Bräuche nennt sich in manchen Gegenden der 15. Hochzeitstag Flaschenhochzeit. Welcher Name auch verwendet wird, eines ist allen gemeinsam: das durchsichtige, zerbrechliche Glas."
     ],
   [
+    "period" => 16,
+    "name" => "Die Saphirhochzeit",
+    "text" => "Allgemeine Bräuche zur Saphirhochzeit gibt es nicht. Weil der 16. Hochzeitstag den Namen eines Edelsteines trägt, ist es in manchen Kreisen üblich, der Ehefrau ein Schmuckstück mit einem blauen Saphir zu schenken. Das ist aber kein Muss, denn schließlich zählt der blaue Saphir zu den teuersten Schmucksteinen und nicht jedermann hat einen wohlgefüllten Geldbeutel. Sicher lässt sich der Hochzeitstag auch ohne ein so teures Geschenk gebührend feiern."
+  ],
+  [
+    "period" => 17,
+    "name" => "Die Orchideenhochzeit",
+    "text" => "Die Orchidee gilt nicht nur als eine sehr wertvolle Blüte, sondern auch als eine Pflanze, die schwer zu pflegen ist. Deswegen hat der Name Orchideenhochzeit wohl zweierlei Bedeutung. Einerseits weist er darauf hin, dass eine Ehe, die 17 Jahre lang gut gegangen ist, als besonders wertvoll betrachtet werden darf. Andererseits macht er gleichzeitig darauf aufmerksam, dass es nicht so einfach ist, den 17. Hochzeitstag zu erreichen. Denn nur wer seine Liebe pflegt, den Partne  respektiert und sich in Notlagen unterstützt, kann die Orchideenhochzeit feiern."
+  ],
+  [
+    "period" => 18,
+    "name" => "Die Türkishochzeit",
+    "text" => "Ebenso, wie der Türkis ein wertvolles Mineral ist, darf auch der 18. Hochzeitstag als selten und wertvoll angesehen werden. Schließlich gibt es nicht viele Ehepaare, die diesen Hochzeitstag miteinander verbringen dürfen. Besondere Bräuche gibt es für die Türkishochzeit nicht. Ein Geschenk, von Verwandten oder Freunden, das zeigt, dass sie an das Jubelpaar denken, wird aber gerne angenommen."
+  ],
+  [
+    "period" => 19,
+    "name" => "Die Perlmutthochzeit",
+    "text" => "Ebenso wie das harte Perlmutt einige Jahre braucht, um seine Widerstandsfähigkeit zu erreichen, hat auch die 19–jährige Ehe einige Zeit benötigt, um ihre jetzige Stabilität zu erreichen. Nach fast einem fünftel Jahrhundert ist die Partnerschaft so fest wie Perlmutt. Die Eheleute verstehen sich ohne Worte und wissen genau, um die Freuden und Leiden des Partners bescheid."
+  ],
+  [
     "period" => 20,
-    "name" => "Porzellan Hochzeit",
-    "text" => "In 20 Jahren Ehe kann im Streit auch mal der ein oder andere Teller aus Porzellan Sprünge bekommen oder sogar zu Bruch gehen. Daher wird dieses Jubiläum in manchen Regionen auch die Dornenhochzeit genannt.\nIm Alltag fällt in jeder Beziehung mal ein Wort, das einen kleinen Riss oder Stich hinterlassen kann. Wie jeder aber weiß sind Porzellan und Dornen so stabil, dass sie nicht zerbrechen. Ebenso hält die Ehe nach 20 Jahren viel aus und bleibt fest und stabil."
-    ],
+    "name" => "Die Porzellan Hochzeit",
+    "text" => "Weil gutes Porzellan teuer und edel ist, wurde es schon früher als "das Gold in Weiß" bezeichnet. Und genau aus diesen Eigenschaften heraus lässt sich auch der Name für das 20. Hochzeitsjubiläum ableiten. Denn schließlich darf eine Ehe, in der sich die Partner bereits seit 20 Jahren treu zur Seite stehen, ebenfalls als teuer und kostbar angesehen werden. Nicht viele Paare erreichen dieses Hochzeitsjubiläum."
+  ],
+  [
+    "period" => 21,
+    "name" => "Die Opal Hochzeit",
+    "text" => "Dass eine Ehe länger als 20 Jahre hält, ist keine Selbstverständlichkeit. Im Gegenteil, eine so lange andauernde Beziehung muss als etwas besonders Wertvolles und Außergewöhnliches betrachtet werden. Sie ist sicherlich ebenso kostbar, wie ein Opal. Althergebrachte Bräuche gibt es zu diesem Jubiläum nicht und auch beim Schmücken des Festtisches und des Festraumes kann man seiner Fantasie freien Lauf lassen."
+  ],
+  [
+    "period" => 22,
+    "name" => "Die Bronze Hochzeit",
+    "text" => "Aus Bronze werden beispielsweise Wandteller gefertigt, die mit einer hübschen Widmung versehen werden können. Aber auch kleine Bronzeskulpturen für die Vitrine oder die Fensterbank eignen sich als Mitbringsel zum 22. Hochzeitstag. Wer sein Geschenk mit einer lustigen Rede an den Mann und die Frau bringen möchte, verschenkt eine Bronzemedaille und macht eine Anspielung auf die zu erwartende Silberhochzeit und die ebenfalls noch bevorstehende Goldene Hochzeit."
+  ],
+  [
+    "period" => 23,
+    "name" => "Die Titan Hochzeit",
+    "text" => "Ob sich der Name Titanhochzeit für den 23. Hochzeitstag von dem gleichnamigen Metall ableiten lässt, oder ob es eher auf das Göttergeschlecht aus der griechischen Mythologie zurückzuführen ist, lässt sich heute nicht mehr klären. Da die Ehe nach 23 Jahren einen beinahe göttlichen Status erreicht hat, und somit ebenso beständig ist, wie Titan, klingen beide Erklärungen einleuchtend."
+  ],
+  [
+    "period" => 24,
+    "name" => "Die Satin Hochzeit",
+    "text" => "Wie der 24. Hochzeitstag zu der Bezeichnung Satinhochzeit kam, lässt sich heute nicht mehr ganz nachvollziehen. Vielleicht lässt sich mit der Festigkeit des Gewebes ein Bezug zur Festigkeit der Ehe schaffen, die nach 24 Jahren sicherlich schon einige Zerreißproben überstehen musste. Vielleicht gibt der Name aber auch einen Hinweis auf die besondere Pflege, die sowohl der empfindliche Satinstoff verlangt, wie auch eine Ehe, die nach so langer Zeit noch immer glücklich und beständig ist."
+  ],
   [
     "period" => 25,
     "name" => "Die Silberhochzeit",
@@ -102,12 +146,12 @@ $periods = [
   [
     "period" => 30,
     "name" => "Die Perlenhochzeit",
-    "text" => "Nach 30 Jahren Ehe feiert man die Perlenhochzeit. Die aneinanderreihenden Perlen einer Kette stehen symbolisch für die aneinanderreihenden Tage der Ehe. Jede Perle, jeder Tag, hat eine eigene Geschichte, so wurden Kinder geboren, vielleicht ein Haus gebaut, das Paar hatte gute und schlechte Zeiten."
+    "text" => "Die Perlen, die auf der Perlenkette aneinandergereiht sind, stehen symbolisch für die vielen sich aneinanderreihenden Tage der Ehe. Jede Perle soll an ein Erlebnis der gemeinsam verbrachten 30 Ehejahre erinnern. Die Bedeutung der sich aneinanderreihenden Perlen weist also einerseits auf die Länge hin, die die Beziehung nun bereits überstanden hat. Und sie zeigt gleichzeitig, dass es sich um eine sehr wertvolle Partnerschaft handelt. So wertvoll wie Perlen."
     ],
   [
     "period" => 35,
     "name" => "Die Leinen Hochzeit",
-    "text" => "Nach 35 Jahren hat die Ehe gezeigt hat, das sie so unzerreißbar ist wie gutes Leinen, aber auch flexibel genug um nicht immer nur starren Regeln zu folgen Das traditionelle Geschenk zu diesem Hochzeitstag ist Wäsche."
+    "text" => "Wie auch alle anderen Bezeichnungen der Hochzeitstage stammt der Name Leinwandhochzeit aus früheren Zeiten. Er ist entstanden, als es die Fotografie noch nicht gab. Denn damals ließen sich die Jubelpaare als Andenken an ihren 35. Hochzeitstag auf Leinwand malen. Die Leinwand wurde gewählt, weil sie aus Leinen bestand und Leinen, beziehungsweise Linnen, ein fester und extrem reißfester Stoff ist. Trotz seiner Robustheit ist Leinen flexibel und lässt sich daher mit seinen typischen Eigenschaften hervorragend als Symbol für eine so lange bestehende Lebensgemeinschaft verwenden."
     ],
   [
     "period" => 40,
